6bca7e787800791cdc607248edb4af88a571a3fe
willtunner <williampereira21@gmail.com>
2022-11-06 19:47:27 -0300

    primeiro commit

---
+++ src/main/java/com/greecode/IndependentBand/IndependentBandApplication.java
@@ -0,0 +1,13 @@
+package com.greecode.IndependentBand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndependentBandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndependentBandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greecode/IndependentBand/IndependentBandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greecode.IndependentBand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndependentBandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
